def76d68e3ce4fdcc56ae501b7ed8df077da61fa
HaiderFazal <haiderfazal363@gmail.com>
2024-10-03 12:19:41 -0400

    Update print('hello world').py

--- print('hello world').py
+++ print('hello world').py
@@ -1,1 +1,18 @@
-print('hello world')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import GridSearchCV
+
+#Step 1 - Data Processing
+df = pd.read_csv("C:/Users/Saira/Downloads/Project_1_Data.csv")
+print(df.info())
+
+#Step 2 - Data Visualization
+
