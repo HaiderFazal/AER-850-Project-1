6e2f6a11c2bd065b96b3704c7e3bdc4345739c60
HaiderFazal <haiderfazal363@gmail.com>
2024-10-06 17:26:40 -0400

    Update print('hello world').py

--- print('hello world').py
+++ print('hello world').py
@@ -9,10 +9,37 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import GridSearchCV
+#from mpl_toolkits.mplot3d import Axes3D
 
 #Step 1 - Data Processing
-df = pd.read_csv("C:/Users/Saira/Downloads/Project_1_Data.csv")
+
+df = pd.read_csv("C:/Users/Saira/Desktop/Uni/Year 4/Sem 1/AER-850\Project_1_Data.csv")
 print(df.info())
 
 #Step 2 - Data Visualization
+
+# 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+# Plotting X, Y, Z against Step
+scatter_plot=ax.scatter(df['X'], df['Y'], df['Z'], c=df['Step'], cmap='magma')
+
+# Labels
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.set_title('Scatter Plot of X,Y,Z against Step')
+cbar = plt.colorbar(scatter_plot,ax=ax)
+cbar.set_label('Step')
+plt.show()
+
+#Step 3 - Correlation Analysis
+corr=df.corr()
+corr=corr.drop(['Step'],axis='columns')
+corr=corr.drop(['Step'],axis='rows')
+sns.heatmap(corr,cmap='rocket',annot=True)
+plt.show()
+
+#Step 4 - Classification Model Development/ Engineering
 
